07157c40afcc2cfd24c7dd20af0e2d8d1e65d982
root <root@DESKTOP-3M83U3L>
2024-11-09 04:39:02 -0500

    Updates

--- scripts/firebaseConfig.js
+++ scripts/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDgDPOBSQ8Vx6wNKUejBfw9YRhvfB35KIY",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  authDomain: "checkmates-a47d9.firebaseapp.com",
+  projectId: "checkmates-a47d9",
+  storageBucket: "checkmates-a47d9.firebasestorage.app",
+  messagingSenderId: "366732803785",
+  appId: "1:366732803785:web:ccfca332ccdea6267fd718",
+  measurementId: "G-1F2K0BT729"
 };
-
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const auth = getAuth(app);
